120e5bdd2250ed44af93b50595fc3f60c9afdaab
Alisher Zhaken <alisherzhaken@gmail.com>
2021-01-26 17:52:36 +0000

    Remove allocator. Change m_data type to unique_ptr

--- array.hpp
+++ array.hpp
@@ -7,46 +7,40 @@
 // VERSION 1.0
 
 namespace functional_cpp {
-    template<class T, class Allocator = std::allocator<T>>
+    template<class T>
     class array {
         using value_type = T;
         using size_type = std::size_t;
-        using pointer = value_type*;
+        using pointer = std::unique_ptr<value_type[]>;
         using reference = value_type&;
         using const_reference = const value_type&;
-        using iterator = pointer;
+        using iterator = value_type*;
         public:
-            array(std::size_t size) : m_allocator{}, m_data{m_allocator.allocate(size)}, m_size{size} {}
-            array(std::initializer_list<T> collection) : m_allocator{}, 
-                                                         m_data{m_allocator.allocate(collection.size())}, 
+            array(std::size_t size) : m_data{std::make_unique<value_type[]>(size)}, m_size{size} {}
+            array(std::initializer_list<T> collection) : m_data{std::make_unique<value_type[]>(collection.size())}, 
                                                          m_size{collection.size()} {
                 for (auto it {collection.begin()}; it != collection.end(); ++it) {
                     m_data[it - collection.begin()] = *it;
                 }
             }
-            ~array() {
-                if (m_data != nullptr) {
-                    m_allocator.deallocate(m_data, m_size);
-                }
-            }
+            ~array() = default;
             reference operator[](size_type index) const {
                 assert(index >= 0 && index < m_size);
                 return m_data[index];
             }
             pointer data() const {
-                return m_data;
+                return m_data.get();
             }
             iterator begin() {
-                return m_data;
+                return m_data.get();
             }
             iterator end() {
-                return m_data + m_size;
+                return m_data.get() + m_size;
             }
             std::size_t size() const {
                 return m_size;
             }
         private:
-            Allocator m_allocator;
             pointer m_data;
             std::size_t m_size;
     };
